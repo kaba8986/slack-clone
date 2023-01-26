3aabf6bfbca052c9a3c977e7149c63b9a31636dc
Brett Scott <brettsamscott@gmail.com>
2023-01-26 21:03:01 +0100

    imported more material design modules for signin componemt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatDatepickerModule } from '@angular/material/datepicker'; 
+import { MatProgressBarModule } from '@angular/material/progress-bar'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatMenuModule } from '@angular/material/menu'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatTooltipModule } from '@angular/material/tooltip'; 
+
+
 import { ChannelComponent } from './channel/channel.component';
 import { RouterModule } from '@angular/router';
-import { MatDialogModule } from '@angular/material/dialog';
+
 import { SigninComponent } from './signin/signin.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -35,6 +46,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     RouterModule,
     MatDialogModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTooltipModule,
     FormsModule,
     ReactiveFormsModule
   ],
