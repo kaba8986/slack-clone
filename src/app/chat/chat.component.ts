27ff8de2c70683c49a9b21627639a37bb3b74c82
Andreas Burghardt <andreas-burghardt@gmx.net>
2023-02-03 11:35:48 +0100

    changed chat-input-layout, format-functions (bold, italic ...) are working

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -7,45 +7,65 @@ import { Component } from '@angular/core';
 })
 export class ChatComponent {
 
-  constructor() {
+  @ViewChild('fileUploader') fileUploader:ElementRef;
+ 
 
+  chatHis: any = [
+    {
+      userName: 'Max Mustermann',
+      timeStamp: '09:31',
+      content: 'Hallo Andreas. Na, wie kommst du voran?'
+    },
+    {
+      userName: 'Andreas Burghardt',
+      timeStamp: '10:04',
+      content: 'Hi Max. Ganz gut gerade. Bin jetzt aktuell beim Slack-Clone, also schon beim letzten Coding-Project. Sieht kompliziert aus, aber bekommt man bestimmt auch irgendwie hin. Bei dir?'
+    },
+    {
+      userName: 'Max Mustermann',
+      timeStamp: '10:12',
+      content: 'Okay cool. Ja auch ganz gut. Hab jetzt das Portfolio gerade zum zweiten mal eingereicht. Irgendwas finden die halt immer :D'
+    },
+    {
+      userName: 'Andreas Burghardt',
+      timeStamp: '10:22',
+      content: 'Haha ja :D War bei mir aber auch so. Ist ja aber auch richtig. Junus und Manuel sagen ja immer wieder in den Videos, dass das Portfolio perfekt sein muss.'
+    },
+    {
+      userName: 'Max Mustermann',
+      timeStamp: '10:36',
+      content: 'Stimmt. Slack-Clone ist wieder so ein Gruppen-Projekt wie Join oder? Nur auf Angular-Basis halt?'
+    },
+    {
+      userName: 'Andreas Burghardt',
+      timeStamp: '10:49',
+      content: 'Genau, Gruppenarbeit. Wir sind gerade am Anfang. Jeder kümmert sich gerade um das HTML und Basis-CSS eines bestimmten Teils, danach überlegen wir uns, wie es weitergeht.'
+    },
+  ]
+
+
+
+  constructor() {}
+
+ 
+
+  formatText(style: string) {
+    document.execCommand(style);
   }
 
-chatHis: any = [
-  {
-    userName: 'Max Mustermann',
-    timeStamp: '09:31',
-    content: 'Hallo Andreas. Na, wie kommst du voran?'
-  },
-  {
-    userName: 'Andreas Burghardt',
-    timeStamp: '10:04',
-    content: 'Hi Max. Ganz gut gerade. Bin jetzt aktuell beim Slack-Clone, also schon beim letzten Coding-Project. Sieht kompliziert aus, aber bekommt man bestimmt auch irgendwie hin. Bei dir?'
-  },
-  {
-    userName: 'Max Mustermann',
-    timeStamp: '10:12',
-    content: 'Okay cool. Ja auch ganz gut. Hab jetzt das Portfolio gerade zum zweiten mal eingereicht. Irgendwas finden die halt immer :D'
-  },
-  {
-    userName: 'Andreas Burghardt',
-    timeStamp: '10:22',
-    content: 'Haha ja :D War bei mir aber auch so. Ist ja aber auch richtig. Junus und Manuel sagen ja immer wieder in den Videos, dass das Portfolio perfekt sein muss.'
-  },
-  {
-    userName: 'Max Mustermann',
-    timeStamp: '10:36',
-    content: 'Stimmt. Slack-Clone ist wieder so ein Gruppen-Projekt wie Join oder? Nur auf Angular-Basis halt?'
-  },
-  {
-    userName: 'Andreas Burghardt',
-    timeStamp: '10:49',
-    content: 'Genau, Gruppenarbeit. Wir sind gerade am Anfang. Jeder kümmert sich gerade um das HTML und Basis-CSS eines bestimmten Teils, danach überlegen wir uns, wie es weitergeht.'
-  },
-]
+  showFilename(event) {
+    let outputField = document.getElementById( 'uploads' );
 
-openDialog() {
-  
-}
+    let input = event.srcElement;
+    let fileName = input.files[0].name;
+
+    outputField.textContent = fileName;
+  }
+
+  resetUpload() {
+    this.fileUploader.nativeElement.value = null;
+    console.log(this.fileUploader.nativeElement.value);
+    document.getElementById( 'uploads' ).textContent = "";
+  }
 
 }
