f9e1341cad6e2c2b10a5fa2d96fec5cc1318974c
HagenStruve <111054317+HagenStruve@users.noreply.github.com>
2023-02-09 21:46:44 +0100

    work on searchFilter

--- src/app/search-filter/search-filter.component.ts
+++ src/app/search-filter/search-filter.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Component, Input, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
 
 @Component({
   selector: 'app-search-filter',
@@ -34,17 +34,26 @@ export class SearchFilterComponent implements OnInit {
     'Batwoman',
   ];
   allThreads: any = [];
+  currentThreadId: string;
+  docRef;
+  docSnap;
+  data: any;
+  allAnswers: any = [];
+  db = getFirestore();
+
+  @Input() channelId: string;
 
   // channelId: string;
 
-  constructor(private route: ActivatedRoute, private firestor: Firestore) {
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore ) {
 
   }
 
   ngOnInit(): void {
     // this.route.params.subscribe( (params): void => {
     // this.getChannelId(params);
-    this.getUpdatesFromChannelCollection();
+    // this.getUpdatesFromChannelCollection();
+    this.getThreadFromServer();
     // this.getDataForThreadService()
     // });
   }
@@ -53,36 +62,48 @@ export class SearchFilterComponent implements OnInit {
   //   this.channelId = params['channelName'];
   // }
 
-  async getUpdatesFromChannelCollection() {
-    // this.firestore.collection('channel').valueChanges({ idField: 'customIdName' }).subscribe((changes) => {
-    //   this.allThreads = this.allThreads.sort(this.sortThreads('originalDate'));
-    //   this.allThreads = changes;
-    //   console.log('json', this.allThreads);
-
-    var channels = collection(this.firestor, 'channel');
-    const q = query(channels, where("Allgemein", "==", true));
-    console.log('json', q);
-
-    const querySnapshot = await getDocs(q);
+  async getThreadFromServer() {
+    const querySnapshot = await getDocs(collection(this.db, "channel"));
     querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-      console.log('das ist', doc);
+      console.log('TESTE', doc.id, " => ", doc.data());
+    });
+
+      this.firestore.collection('channel').doc(this.value).collection('threads').valueChanges().subscribe((changes) => {
+        // this.docRef = doc(this.db, 'channel', this.channelId, 'threads', this.currentThreadId)
+        // this.docSnap = await getDoc(this.docRef);
+        // this.data = this.docSnap.data();
+        // this.allAnswers = this.data.answers;
+        // console.log('WAS', this.docRef);
+        this.allThreads = changes;
+      console.log('json', this.allThreads);
       });
-    }
+  }
 
-  // getDataForThreadService() {
-  //   this.threadContent.channelId = this.channelId;
-  // }
+  // async getUpdatesFromChannelCollection() {
+  //   this.firestore.collection('channel').valueChanges({ idField: 'customIdName' }).subscribe((changes) => {
+  //     this.allThreads = changes;
+  //     console.log('json', this.allThreads);
+  //   });
 
-  // sortThreads(originalDate) {
-  //   return function (a, b) {
-  //     if (a[originalDate] > b[originalDate]) {
-  //       return 1;
-  //     } else if (a[originalDate] < b[originalDate]) {
-  //       return -1;
-  //     }
-  //     return 0;
-  //   }
-  // }
-}
+    // let channels = collection(this.firestor, 'channel');
+    // let q = query(channels, where("Allgemein", "==", true));
+    // console.log('json', q);
+
+    // let querySnapshot = await getDocs(q);
+    // querySnapshot.forEach((doc) => {
+    //   // doc.data() is never undefined for query doc snapshots
+    //   console.log(doc.id, " => ", doc.data());
+    //   console.log('das ist', doc);
+    // });
+
+    // const channels = query(collectionGroup(this.firestor, 'channel'), where('type', '==', 'museum'));
+    // const querySnapshot = await getDocs(channels);
+    // querySnapshot.forEach((doc) => {
+    //   console.log(doc.id, ' => ', doc.data());
+    // });
+
+    // getDataForThreadService() {
+    //   this.threadContent.channelId = this.channelId;
+    // }
+  }
