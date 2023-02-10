d0c8ce1193295ae81e5bbe3bd237e63733ecdee7
Andreas Burghardt <andreas-burghardt@gmx.net>
2023-02-10 11:20:31 +0100

    deleting messages from chat works

--- src/app/delete-chat-warning/delete-chat-warning.component.ts
+++ src/app/delete-chat-warning/delete-chat-warning.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { arrayRemove, arrayUnion, deleteField, doc, getDoc, getFirestore, updateDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-delete-chat-warning',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class DeleteChatWarningComponent {
 
+  chatroomId: string;
+  messageIndex: number;
+  db = getFirestore();
+  allMessages: any;
+
+  constructor(private firestore: AngularFirestore) {
+
+  }
+
+  async deleteMessage(index: number) {
+    const messageRef = doc(this.db, "chats", this.chatroomId);
+    const docSnap = await getDoc(messageRef);
+    this.allMessages = docSnap.data();
+    this.allMessages.messages.splice(index, 1);
+
+    //delete old messages
+    await updateDoc(messageRef, {
+      messages: deleteField()
+    });
+
+    //upload new messages
+    await updateDoc(messageRef, {
+      messages: this.allMessages.messages
+    });
+  }
 }
